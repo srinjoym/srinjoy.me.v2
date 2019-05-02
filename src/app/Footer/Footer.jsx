8721039a7f5ac4063a59991a67f5b85758a10521
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2019-05-02 13:18:41 -0500

    clean up icons and add twitter

--- src/app/Footer/Footer.jsx
+++ src/app/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import theme from './Footer.scss'
 import { Row, Col } from 'react-flexbox-grid/lib/'
+import { FaTwitter, FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'
+
 import Container from '../Shared/Container/Container.jsx'
-import { GithubIcon, LinkedInIcon, EmailIcon } from '../Shared/Icons'
 
 class Footer extends React.Component {
   render () {
@@ -15,9 +16,10 @@ class Footer extends React.Component {
             </Col>
             <Col xs={12} md={6}>
               <ul className={theme.list}>
-                <li><a href='mailto:srinjoy.majumdar@gmail.com'><EmailIcon /></a></li>
-                <li><a href='https://github.com/srinjoym'><GithubIcon /></a></li>
-                <li><a href='http://www.linkedin.com/pub/srinjoy-majumdar/75/913/345/'><LinkedInIcon /></a></li>
+                <li><a href='https://twitter.com/SrinjoyMajumdar'><FaTwitter /></a></li>
+                <li><a href='http://www.linkedin.com/pub/srinjoy-majumdar/75/913/345/'><FaLinkedin /></a></li>
+                <li><a href='https://github.com/srinjoym'><FaGithub /></a></li>
+                <li><a href='mailto:srinjoy.majumdar@gmail.com'><FaEnvelope /></a></li>
               </ul>
             </Col>
           </Row>
--- src/app/Home/Landing/Landing.jsx
+++ src/app/Home/Landing/Landing.jsx
@@ -3,11 +3,10 @@ import { Row, Col } from 'react-flexbox-grid/lib/'
 import ReactGA from 'react-ga'
 import Link from 'react-toolbox/lib/link'
 import ReactDipper from 'react-dipper'
-import Tooltip from 'react-toolbox/lib/tooltip'
+import { FaTwitter, FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'
 
 import theme from './Landing.scss'
 import ScrollLink from '../../Shared/ScrollLink/ScrollLink.jsx'
-import { GithubIcon, LinkedInIcon, EmailIcon } from '../../Shared/Icons.jsx'
 import ParticleConfig from './dipper_config.json'
 
 // Landing page with react dipper background
@@ -32,9 +31,10 @@ class Landing extends React.Component {
               <h1 className={theme.h1}>Hi! I'm Srinjoy</h1>
               <h2 className={theme.h2}>Full Stack Developer and Computer Vision Researcher</h2>
               <ul className={theme.list}>
-                <li><a href='mailto:srinjoy.majumdar@gmail.com'><EmailIcon /></a></li>
-                <li><a href='https://github.com/srinjoym'><GithubIcon /></a></li>
-                <li><a href='http://www.linkedin.com/pub/srinjoy-majumdar/75/913/345/'><LinkedInIcon /></a></li>
+                <li><a href='mailto:srinjoy.majumdar@gmail.com'><FaEnvelope /></a></li>
+                <li><a href='https://github.com/srinjoym'><FaGithub /></a></li>
+                <li><a href='http://www.linkedin.com/pub/srinjoy-majumdar/75/913/345/'><FaLinkedin /></a></li>
+                <li><a href='https://twitter.com/SrinjoyMajumdar'><FaTwitter /></a></li>
               </ul>
             </Col>
           </Row>
