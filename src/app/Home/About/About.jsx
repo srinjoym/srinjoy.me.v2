afac9914c45361260d79c8554a803044e30e5475
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2019-05-28 07:48:35 +0530

    update profile image and about blurb

--- src/app/Home/About/About.jsx
+++ src/app/Home/About/About.jsx
@@ -20,12 +20,11 @@ class About extends React.Component {
               <Col xs={12} md={8}>
                 <p className={theme.p}>
                   I’m interested in solving some of the world’s biggest problems.
-                  To do that, I’m currently learning from some of the brightest at the University of Texas at Austin,
-                  where I am studying Electrical and Computer Engineering. Most recently, I’ve been tackling the issue
-                  of quality programming education as a Junior Software Developer at GitHub. I’m also working on solving
-                  novel Augmented Reality and Human-Robot Interaction problems under Dr. Andrea Thomaz and Dr. Scott Niekum.
-                  I grew up in San Jose California, but I’m hoping I’m able to make people’s lives all over the world better
-                  in the future.
+                  To do that, I’m currently in my final year studying Electrical and Computer Engineering at the University
+                  of Texas at Austin. Most recently, I’ve been tackling the issue of quality software programming education 
+                  as a part-time Junior Software Developer at GitHub. I’m also working on solving novel Augmented Reality 
+                  and Human-Robot Interaction problems under Dr. Andrea Thomaz and Dr. Scott Niekum. I grew up in San Jose
+                  California, but I’m hoping I’m able to make people’s lives all over the world better in the future.
                 </p>
               </Col>
             </Row>
